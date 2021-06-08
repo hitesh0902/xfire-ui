c1c56a1fdd133a0478bce8a4c5cec80d9ef39ff3
xjitenx <38421858+xjitenx@users.noreply.github.com>
2021-06-08 10:21:17 +0530

    added radio button and textField component and its tests in exampleRoute (#1)
    * added radio button and TextField component and its tests in ExampleRoute.

---
+++ src/components/Core/XFTextField.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+interface XFTextFieldProps {
+  label?: string;
+  placeHolder?: string;
+  onChange(e: React.ChangeEvent<HTMLInputElement>): void;
+  disabled?: boolean;
+}
+
+const XFTextField: React.FC<XFTextFieldProps> = ({
+  label,
+  placeHolder,
+  disabled = false,
+  onChange,
+}) => {
+  return (
+    <div>
+      {label && <label className='mr-2'>{label}</label>}
+      <input
+        placeholder={placeHolder}
+        onChange={(e) => onChange(e)}
+        disabled={disabled}
+      />
+    </div>
+  );
+};
+
+export default XFTextField;
--- src/routes/ExampleRoute.tsx
+++ src/routes/ExampleRoute.tsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../types';
 import { changeNumber, changeNumberAsync } from '../slice/ExampleSlice';
 import XFButton from '../components/Core/XFButton';
+import XFTextField from '../components/Core/XFTextField';
+import XFRadioButton from '../components/Core/XFRadioButton';
 
 interface ExampleRouteProps {}
 
 const ExampleRoute: React.FC<ExampleRouteProps> = () => {
+  const [name, setName] = useState('No Name');
+
+  const [sport, setSport] = useState('Nothing');
+
   const dispatch = useDispatch();
   const id = useSelector((state: RootState) => state.example.id);
+
+  const setNameEvent = (event: any) => {
+    setName(event.target.value);
+  };
+
+  const sportsSelected = (event: any) => {
+    setSport(event.target.value);
+  };
 
   return (
     <div>
       <p>Hello</p>
+      <p>My name is {name}</p>
+      <p>My Fav sport is {sport}</p>
       <p>Current ID = {id}</p>
       <XFButton
         variant='primary'
@@ -38,6 +54,16 @@ const ExampleRoute: React.FC<ExampleRouteProps> = () => {
       >
         Subtract
       </XFButton>
+      <XFTextField
+        label='name'
+        placeHolder='Enter your name'
+        onChange={setNameEvent}
+      />
+      <XFRadioButton
+        groupName='sports'
+        values={['football', 'basketball', 'cricket', 'volleyball']}
+        onChange={sportsSelected}
+      />
     </div>
   );
 };
