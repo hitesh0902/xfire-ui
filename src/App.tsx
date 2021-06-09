83568f82230a0b0961929dd6f32cb8b5d3ae0a17
Hitesh Rawat <r.hitesh@fabstudio.co>
2021-06-09 10:33:16 +0530

    auth routes setup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ExampleRoute from './routes/ExampleRoute';
+import LoginRoute from './routes/auth/LoginRoute';
+import RegisterRoute from './routes/auth/RegisterRoute';
+import ForgetPasswordRoute from './routes/auth/ForgetPasswordRoute';
+import Error404Route from './routes/Error404Route';
 
 const App: React.FC = () => {
   return (
@@ -8,6 +12,10 @@ const App: React.FC = () => {
       <BrowserRouter>
         <Switch>
           <Route exact path='/' component={ExampleRoute} />
+          <Route exact path='/auth/login' component={LoginRoute} />
+          <Route exact path='/auth/register' component={RegisterRoute} />
+          <Route exact path='/auth/forget' component={ForgetPasswordRoute} />
+          <Route path='/' component={Error404Route} />
         </Switch>
       </BrowserRouter>
     </div>
---
+++ src/routes/auth/ForgetPasswordRoute.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ForgetPasswordRoute = () => {
+  return (
+    <div>
+      <h1 className='text-4xl'>Forget Everything</h1>
+    </div>
+  );
+};
+
+export default ForgetPasswordRoute;
