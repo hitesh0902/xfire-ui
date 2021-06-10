d5a62097482f30be6b92efa8d9ec28725d03c006
Hitesh Rawat <r.hitesh@fabstudio.co>
2021-06-10 10:45:46 +0530

    add register route and button type

--- src/routes/auth/RegisterRoute.tsx
+++ src/routes/auth/RegisterRoute.tsx
@@ -1,10 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+import XFTextField from '../../components/Core/XFTextField';
+import XFButton from '../../components/Core/XFButton';
 
 const RegisterRoute = () => {
+  const history = useHistory();
+
+  const [name, setName] = useState('');
+  const [userId, setUserId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = () => {
+    console.log(name, userId, password);
+    axios
+      .post('https://localhost:44362/api/Auth/register', {
+        id: userId,
+        password: password,
+      })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((msg) => console.log(msg));
+  };
+
   return (
-    <div>
-      <h1 className='text-4xl'>Register Route</h1>
-    </div>
+    <form
+      onSubmit={handleSubmit}
+      className='flex items-center justify-center h-screen w-full'
+    >
+      <div className='p-4 border border-black'>
+        <h1 className='text-4xl mb-3'>X-Fire Register</h1>
+        <div>
+          <XFTextField
+            placeHolder='Enter your name'
+            type='text'
+            onChange={(e) => setName(e.target.value)}
+          />
+          <XFTextField
+            placeHolder='Enter your user-id or email-id'
+            onChange={(e) => setUserId(e.target.value)}
+          />
+          <XFTextField
+            placeHolder='Enter your password'
+            type='password'
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className='flex justify-center my-3'>
+          <XFButton type='submit'>Submit</XFButton>
+        </div>
+
+        <div className='flex justify-center'>
+          <XFButton onClick={() => history.push('/auth/login')}>
+            Go to Login
+          </XFButton>
+        </div>
+      </div>
+    </form>
   );
 };
 
